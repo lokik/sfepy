b21c83f82ea9e9871cfe0a6096b733371ce6d4f0
Matyas Novak <novakmat@fzu.cz>
2017-04-12 14:46:26 +0200

    Default mesh argument for HDF5MeshIo.read()

--- sfepy/discrete/fem/meshio.py
+++ sfepy/discrete/fem/meshio.py
@@ -1258,7 +1258,7 @@ class HDF5MeshIO(MeshIO):
                 fd.close()
                 return bbox
 
-    def read(self, mesh, **kwargs):
+    def read(self, mesh = None, **kwargs):
         return self.read_mesh_from_hdf5(self.filename, '/mesh', mesh = mesh)
 
     @staticmethod
